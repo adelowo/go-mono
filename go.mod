5a583e5b91b088fc65934bbb63b250520cc65ad7
Lanre Adelowo <yo@lanre.wtf>
2024-09-07 14:30:44 +0100

    implement transactions endpoints (#5)
    * implement transactions endpoint
    
    * add income endpoint

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/adelowo/go-mono
 go 1.23.0
 
 require github.com/oklog/ulid/v2 v2.1.0
+
+require github.com/google/go-querystring v1.1.0
---
+++ mono/transactions.go
@@ -0,0 +1,136 @@
+package mono
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/google/go-querystring/query"
+)
+
+const (
+	// We do not use this internally but can be helpful for people who want to parse the date
+	// correctly
+	DateFormat = "2006-01-02"
+)
+
+type TransactionService service
+
+// ENUM(debit,credit)
+type TransactionType string
+
+type TransactionsFilter struct {
+	Paginate   bool            `url:"paginate,omitempty"`
+	Start, End string          `url:"start,omitempty"`
+	Narration  string          `url:"narration,omitempty"`
+	Type       TransactionType `url:"type,omitempty"`
+	Limit      int             `url:"limit,omitempty"`
+}
+
+type Transaction struct {
+	ID        string    `json:"id"`
+	Narration string    `json:"narration"`
+	Amount    int       `json:"amount"`
+	Type      string    `json:"type"`
+	Balance   int       `json:"balance"`
+	Date      time.Time `json:"date"`
+	Category  string    `json:"category"`
+}
+
+type TransactionMetadata struct {
+	Total    int    `json:"total"`
+	Page     int    `json:"page"`
+	Previous any    `json:"previous"`
+	Next     string `json:"next"`
+}
+
+type TransactionsResponse struct {
+	BaseMonoResponse
+	Data []Transaction       `json:"data"`
+	Meta TransactionMetadata `json:"meta"`
+}
+
+func (t *TransactionService) All(ctx context.Context,
+	accountID string,
+	opts TransactionsFilter) ([]Transaction, TransactionMetadata, error) {
+
+	var resp []Transaction
+	var metadata TransactionMetadata
+
+	body, err := ToReader(NoopRequestBody{})
+	if err != nil {
+		return resp, metadata, nil
+	}
+
+	v, err := query.Values(opts)
+	if err != nil {
+		return resp, metadata, err
+	}
+
+	req, err := t.client.newRequest(http.MethodGet,
+		fmt.Sprintf("/accounts/%s/transactions?%s", accountID, v.Encode()),
+		body)
+	if err != nil {
+		return resp, metadata, nil
+	}
+
+	var response TransactionsResponse
+	_, err = t.client.Do(ctx, req, response)
+	return response.Data, response.Meta, err
+}
+
+type heatmapResponse struct {
+	Total             int `json:"total"`
+	TransactionsCount int `json:"transactions_count"`
+	History           []struct {
+		Period            string `json:"period"`
+		Amount            int    `json:"amount"`
+		TransactionsCount int    `json:"transactions_count"`
+	} `json:"history"`
+}
+
+type InflowResponse = heatmapResponse
+type OutflowResponse = heatmapResponse
+
+func (t *TransactionService) Credits(ctx context.Context, accountID string) (
+	InflowResponse, error) {
+
+	var resp InflowResponse
+
+	body, err := ToReader(NoopRequestBody{})
+	if err != nil {
+		return resp, nil
+	}
+
+	req, err := t.client.newRequest(http.MethodGet,
+		fmt.Sprintf("/accounts/%s/credits", accountID),
+		body)
+	if err != nil {
+		return resp, nil
+	}
+
+	_, err = t.client.Do(ctx, req, resp)
+	return resp, err
+}
+
+func (t *TransactionService) Debits(ctx context.Context, accountID string) (
+	OutflowResponse, error) {
+
+	var resp OutflowResponse
+
+	body, err := ToReader(NoopRequestBody{})
+	if err != nil {
+		return resp, nil
+	}
+
+	req, err := t.client.newRequest(http.MethodGet,
+		fmt.Sprintf("/accounts/%s/debits", accountID),
+		body)
+	if err != nil {
+		return resp, nil
+	}
+
+	_, err = t.client.Do(ctx, req, resp)
+	return resp, err
+}
